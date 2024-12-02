1e3089dbd8d7e025800618366a26f7654a1b9c15
HABIB UR RAHMAN <78023625+habib1710001@users.noreply.github.com>
2024-12-02 14:44:47 -0700

    Updated code

--- sdram_interfacin/Boot/Core/Src/main.c
+++ sdram_interfacin/Boot/Core/Src/main.c
@@ -64,7 +64,7 @@ static void MX_FMC_Init(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-uint32_t *externalRAM = 0xD000000;
+uint32_t *externalRAM = SDRAM_ADDRESS_START;
 uint8_t sdramStatus;
 
 
@@ -128,6 +128,8 @@ int main(void)
   /* USER CODE BEGIN 2 */
 
   FMC_WRITE();
+  HAL_Delay(100);
+
 
   /* USER CODE END 2 */
 
@@ -333,24 +335,44 @@ static void MX_FMC_Init(void)
     FMC_SDRAM_CommandTypeDef Command;
     /* Step 1 and Step 2 already done in HAL_SDRAM_Init() */
     /* Step 3: Configure a clock configuration enable command */
+
+    //Initializes the SDRAM clock to ensure proper synchronization
      Command.CommandMode            = FMC_SDRAM_CMD_CLK_ENABLE; /* Set MODE bits to "001" */
      Command.CommandTarget          = FMC_SDRAM_CMD_TARGET_BANK2; /* configure the Target Bank bits */
      Command.AutoRefreshNumber      = 1;
      Command.ModeRegisterDefinition = 0;
      HAL_SDRAM_SendCommand(&hsdram2, &Command, 0xfff);
      HAL_Delay(1); /* Step 4: Insert 100 us minimum delay - Min HAL Delay is 1ms */
+
+
      /* Step 5: Configure a PALL (precharge all) command */
+     //Precharges all rows in the SDRAM
      Command.CommandMode            = FMC_SDRAM_CMD_PALL; /* Set MODE bits to "010" */
      HAL_SDRAM_SendCommand(&hsdram2, &Command, 0xfff);
+
+
      /* Step 6: Configure an Auto Refresh command */
+     //Initializes the auto-refresh feature of SDRAM.
      Command.CommandMode            = FMC_SDRAM_CMD_AUTOREFRESH_MODE; /* Set MODE bits to "011" */
      Command.AutoRefreshNumber      = 2;
      HAL_SDRAM_SendCommand(&hsdram2, &Command, 0xfff);
+
      /* Step 7: Program the external memory mode register */
+     /*Configures the SDRAM's mode register for key settings like CAS latency, burst length, and burst type
+     /*
+     Burst Length (BL): 0 (1 burst).
+     Burst Type (BT): 0 (sequential).
+     CAS Latency (CL): 2 (CAS latency = 2 cycles).
+     Operating Mode: 0 (Standard operation).
+     Write Burst Mode: 1 (Single location).
+     */
      Command.CommandMode            = FMC_SDRAM_CMD_LOAD_MODE;/*set the MODE bits to "100" */
      Command.ModeRegisterDefinition =  (uint32_t)0 | 0<<3 | 2<<4 | 0<<7 | 1<<9;
      HAL_SDRAM_SendCommand(&hsdram2, &Command, 0xfff);
+
+
      /* Step 8: Set the refresh rate counter - refer to section SDRAM refresh timer register in RM0455 */
+     /*Configures the SDRAM refresh rate to avoid data loss due to charge leakage.
      /* Set the device refresh rate
       * COUNT = [(SDRAM self refresh time / number of row) x  SDRAM CLK] – 20
               = [(64ms/4096) * 100MHz] - 20 = 1562.5 - 20 ~ 1542 */
